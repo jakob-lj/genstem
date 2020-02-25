67fc84051963e8161daae8994b9d92d3d4e7b122
henmyhre <56293655+henmyhre@users.noreply.github.com>
2020-02-25 22:49:17 +0100

    moved containers to independent file

--- src/components/home/index.jsx
+++ src/components/home/index.jsx
@@ -1,24 +1,25 @@
 import React, { Component } from "react";
 import "./style_home.css";
 import { Link } from "react-router-dom";
+import Container from "./../Container";
 
 export default class Home extends Component {
   render() {
     return (
       <div>
         <h1 className="header1">Genstem</h1>
-        <div className="container1">
+        <Container>
           <Link to={"/join"}>
             <button className="button1" type="button">
               Join
             </button>
           </Link>
-          <Link to={'/create'}>
+          <Link to={"/create"}>
             <button className="button2" type="button">
               Create
             </button>
           </Link>
-        </div>
+        </Container>
       </div>
     );
   }
--- src/components/join/index.jsx
+++ src/components/join/index.jsx
@@ -1,21 +1,19 @@
 import React, { Component } from "react";
 
+import Container from "./../Container";
+
 import "./style_join.css";
 
 export default class Join extends Component {
-
   login() {
-    console.log('login');
-
+    console.log("login");
   }
 
   render() {
     return (
       <div>
         <h1 className="header">Genstem</h1>
-        <div className="container">
-
-        </div>
+        <Container></Container>
       </div>
     );
   }
