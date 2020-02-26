2cc81960e97afc089e62f8841e2d705418585445
Jakob Lund Johannessen <jakob-lj@hotmail.com>
2020-02-26 12:31:43 +0100

    added login logic

--- src/components/create/index.jsx
+++ src/components/create/index.jsx
@@ -3,6 +3,7 @@ import Input from "./../inputs";
 import "./style_create.css";
 import { post } from "./../../Network/client.js";
 import Container from "./../Container";
+import {Redirect} from 'react-router-dom';
 
 export default class Create extends Component {
   constructor(props) {
@@ -11,7 +12,9 @@ export default class Create extends Component {
       loginPage: 0,
       loading: false,
       showError: false,
-      error: ""
+      error: "",
+      userId: null,
+      redirect: null
     };
     this.login = this.login.bind(this);
     this.loginWithSSOCode = this.loginWithSSOCode.bind(this);
@@ -24,26 +27,38 @@ export default class Create extends Component {
     this.setState({ loading: true });
     post("/auth/login/", { email }, false).then(r => {
       if (r.ok) {
-        this.setState({ loginPage: 1, showError: false, loginId: r.id });
+        this.setState({ loginPage: 1, showError: false, userId: r.id,
+        loading: false });
       } else if (r.err === "NOT_A_USER") {
         this.setState({
           loginPage: 0,
           showError: true,
-          error: "Finner ingen registrert bruker med denne e-posten"
+          error: "Finner ingen registrert bruker med denne e-posten",
+          loading: false
         });
       } else {
         this.setState({
           loginPage: 0,
           showError: true,
-          error: "Det skjedde en feil"
+          error: "Det skjedde en feil",
+          loading: false
         });
       }
     });
-    console.log(this.state);
   }
 
   loginWithSSOCode() {
     console.log("yey");
+    let code = document.getElementById('SSOCode').value;
+    let id = this.state.userId;
+    post('/auth/token/', {id, code}, false).then(r => {
+      if (r.ok) { // user got logged in
+        localStorage.setItem('genstemToken', r.tokens.access);
+        this.setState({redirect: '/host'})
+      } else if (r.err === 'CODE_EXPIRED') {
+        this.setState({showError: true, error: 'Koden er ikke riktig, eller ikke gyldig lenger', loading: false});
+      }
+    });
   }
 
   Feedback(props) {
@@ -55,7 +70,6 @@ export default class Create extends Component {
         display: "none"
       };
     }
-    console.log(style);
     return (
       <div style={style}>
         <div className="error">
@@ -85,14 +99,13 @@ export default class Create extends Component {
     if (props.currentState !== 1) {
       return null;
     }
-    console.log('viewone');
     return (
       <div>
         <div>
           <Input placeholder={"Engangskode"} id={"SSOCode"} />
         </div>
         <button
-          onClick={props.loginWithSSOCode}
+          onClick={props.login}
           className="button"
           type="button">
           Enter
@@ -111,6 +124,11 @@ export default class Create extends Component {
   }
 
   render() {
+
+    if (this.state.redirect !== null) {
+      return <Redirect to={this.state.redirect} />
+    }
+
     // {views[this.state.loginPage]()}
     return (
       <div>
