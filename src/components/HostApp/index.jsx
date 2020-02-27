be8e6406f6668adf7cc7b0b4eaa7edd3d52d73e1
Jakob Lund Johannessen <jakob-lj@hotmail.com>
2020-02-27 02:04:15 +0100

    added event map to /host

--- src/components/HostApp/index.jsx
+++ src/components/HostApp/index.jsx
@@ -5,11 +5,13 @@ import { get } from '../../Network/client';
 export default function Host(props) {
 
     let [loading, setLoading] = useState(true);
+    let [events, setEvents] = useState([]);
 
     useEffect(() => {
         get('/events/').then(r => {
-            console.log(r);
+            console.log(r); // remove when sure what it does
             setLoading(false);
+            setEvents(r);
         });
     }, []);
 
@@ -19,7 +21,12 @@ export default function Host(props) {
         </Container>
     }
 
+    let eventsDiv = events.map(event => {
+        return <div key={event.id}>{event.name} - {event.active ? 'active' : 'inactive'} - {event.startDate}</div>
+    });
+
     return <Container>
-        <p>This is host</p>
+        <button onClick={() => alert('implement - and style better pllllz')}>Legg til arrangement</button>
+        {eventsDiv}
     </Container>;
 }
